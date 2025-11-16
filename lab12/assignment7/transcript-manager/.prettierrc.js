c2de68e5ef9fc804821f3097488670b04403a350
ibadlayevam19 <malika19102005@gmail.com>
2025-11-16 20:55:48 +0500

    0 errors

--- lab12/assignment7/transcript-manager/.prettierrc.js
+++ lab12/assignment7/transcript-manager/.prettierrc.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 module.exports = {
   printWidth: 100,
   tabWidth: 2,
--- lab12/assignment7/transcript-manager/jest.config.js
+++ lab12/assignment7/transcript-manager/jest.config.js
@@ -1,4 +1,5 @@
 // jest.config.js
+/* eslint-disable no-undef */
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
